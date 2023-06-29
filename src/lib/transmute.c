ff32923488c88f457d86e0ea66e8cec585a16655
Peter Bjorklund <piot@hotmail.com>
2023-06-29 14:33:59 +0200

    fix: compile warnings and use generated cmake file

--- src/lib/transmute.c
+++ src/lib/transmute.c
@@ -6,8 +6,8 @@
 #include <transmute/transmute.h>
 
 /// Initializes a transmute virtual machine
-/// @param self
-/// @param vmPointer
+/// @param self virtual machine
+/// @param vmPointer application specific pointer
 /// @param setup contains tick, getState and setState functions
 void transmuteVmInit(TransmuteVm* self, void* vmPointer, TransmuteVmSetup setup, Clog log)
 {
@@ -25,8 +25,8 @@ void transmuteVmInit(TransmuteVm* self, void* vmPointer, TransmuteVmSetup setup,
 
 /// Sets a state to the virtual machine
 /// Replaces the internal cached state with the specified state.
-/// @param self
-/// @param state
+/// @param self  virtual machine
+/// @param state application specific state to be set
 void transmuteVmSetState(TransmuteVm* self, const TransmuteState* state)
 {
     self->setStateFn(self->vmPointer, state);
@@ -35,14 +35,14 @@ void transmuteVmSetState(TransmuteVm* self, const TransmuteState* state)
 
 /// Ticks the virtual machine. The virtual machine keeps the mutated state in an efficient way
 /// that cache must not affect the determinism
-/// @param self
-/// @param input
+/// @param self  virtual machine
+/// @param input transmute input for this tick
 void transmuteVmTick(TransmuteVm* self, const TransmuteInput* input)
 {
     if (!self->initialStateIsSet) {
         CLOG_ERROR("can not tick, since initial state has not been set")
     }
-#if TRANSMUTE_LOG_TICK
+#if defined TRANSMUTE_LOG_TICK
     CLOG_C_VERBOSE(&self->log, "tick with inputs:")
     char inputDebugString[64];
     for (size_t i=0; i<input->participantCount; ++i) {
@@ -55,8 +55,8 @@ void transmuteVmTick(TransmuteVm* self, const TransmuteInput* input)
 }
 
 /// Gets the transmute state from the virtual machine internal representation.
-/// @param self
-/// @return
+/// @param self virtual machine
+/// @return the application specific transmute state
 TransmuteState transmuteVmGetState(const TransmuteVm* self)
 {
     if (!self->initialStateIsSet) {
@@ -66,50 +66,53 @@ TransmuteState transmuteVmGetState(const TransmuteVm* self)
 }
 
 /// Convert a TransmuteState to a string
-/// @param self
-/// @param state
-/// @param target
-/// @param maxTargetOctetSize
-/// @return
+/// @param self virtual machine
+/// @param state the application specific state
+/// @param target char target buffer to receive the debug string
+/// @param maxTargetOctetSize maximum size of the target buffer
+/// @return the number of characters written
 int transmuteVmStateToString(const TransmuteVm* self, const TransmuteState* state, char* target, size_t maxTargetOctetSize)
 {
     return self->stateToString(self->vmPointer, state, target, maxTargetOctetSize);
 }
 
 /// Convert a Transmute
-/// @param self
-/// @param input
-/// @param target
-/// @param maxTargetOctetSize
-/// @return
+/// @param self  virtual machine
+/// @param input input for the participants
+/// @param target char target buffer to receive the debug string
+/// @param maxTargetOctetSize maximum size of the target buffer
+/// @return the number of characters written
 int transmuteVmInputToString(const TransmuteVm* self, const TransmuteParticipantInput* input, char* target, size_t maxTargetOctetSize)
 {
     return self->inputToString(self->vmPointer, input, target, maxTargetOctetSize);
 }
 
 /// Checks if a state has been set to the virtual machine
-/// @param self
-/// @return
+/// @param self virtual machine
+/// @return true if a state has been set at some time
 bool transmuteVmHasState(const TransmuteVm* self)
 {
     return self->initialStateIsSet;
 }
 
 /// Checks that versions are equal
-/// @param a
-/// @param b
-/// @return
+/// @param a version
+/// @param b version
+/// @return true if equal
 bool transmuteVmVersionIsEqual(const TransmuteVmVersion* a, const TransmuteVmVersion* b)
 {
     return a->major == b->major && a->minor == b->minor && a->patch == b->patch;
 }
 
-
+/// Finds the index given a participant id
+/// @param input transmute input
+/// @param participantId the participantId to search for
+/// @return index or negative if failed
 int transmuteInputFindParticipantId(const TransmuteInput* input, uint8_t participantId)
 {
     for (size_t i=0; i< input->participantCount; ++i) {
         if (input->participantInputs[i].participantId == participantId) {
-            return i;
+            return (int)i;
         }
     }
 
